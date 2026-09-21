9916e771a975e7678a6f54a0d9e0b4e297cececc
Dario Quispe <daro.qp@gmail.com>
2026-09-20 23:20:26 -0300

    refactor: se elimino la implementacion Item, se puso en opcional descripcion y se inicializo en PENDIENTE el estado

--- src/Producto.ts
+++ src/Producto.ts
@@ -1,70 +1,63 @@
 import { CategoriaProd } from "./CategoriaProd";
 import { EstadoProducto } from "./EstadoProducto";
-import { Item } from "./Item";
 
-export class Producto implements Item {
+export class Producto {
 
-    private categoria: CategoriaProd;
-    private nombre: string;
-    private precio:number ;
-    private descripcion:string;
-    private estado:EstadoProducto;
+  private nombre: string;
+  private precio: number;
+  private descripcion: string;
+  private estado: EstadoProducto;
+  private categoria: CategoriaProd;
 
-    public constructor(categoria: CategoriaProd,
-         nombre: string, descripcion:string, 
-         precio: number,estado:EstadoProducto) {
-        
-            this.categoria = categoria;
-            this.nombre = nombre;
-            this.precio = precio;
-            this.descripcion = descripcion;
-            this.estado = estado;
-    }
+  public constructor(nombre: string,
+    precio: number,
+    categoria: CategoriaProd,
+    descripcion?: string) {
 
-    public getCategoria():CategoriaProd {
-        return this.categoria;
-    }
+    this.nombre = nombre;
+    this.precio = precio;
+    this.categoria = categoria;
+    this.descripcion = descripcion ?? "";
+    this.estado = EstadoProducto.PENDIENTE;
+  }
 
-    public setCategoria(categoria:CategoriaProd):void {
-        this.categoria = categoria;
-    }
+  public getCategoria(): CategoriaProd {
+    return this.categoria;
+  }
 
-    public getNombre():string {
-        return this.nombre;
-    }
+  public setCategoria(categoria: CategoriaProd): void {
+    this.categoria = categoria;
+  }
 
-    public setNombre(nombre:string):void {
-        this.nombre = nombre;
-    }
+  public getNombre(): string {
+    return this.nombre;
+  }
 
-    public getPrecio():number {
-        return this.precio;
-    }
+  public setNombre(nombre: string): void {
+    this.nombre = nombre;
+  }
 
-    public setPrecio(precio:number):void {
-        this.precio = precio;
-    }
+  public getPrecio(): number {
+    return this.precio;
+  }
 
-    public getDescripcion():string {
-        return this.descripcion;
-    }
+  public setPrecio(precio: number): void {
+    this.precio = precio;
+  }
 
-    public setDescripcion(descripcion:string):void {
-        this.descripcion = descripcion;
-    }
+  public getDescripcion(): string {
+    return this.descripcion;
+  }
 
-    public getEstado():EstadoProducto {
-        return this.estado;
-    }
+  public setDescripcion(descripcion: string): void {
+    this.descripcion = descripcion;
+  }
 
-    public setEstado(estado:EstadoProducto):void {
-        this.estado = estado;
-    }
+  public getEstado(): EstadoProducto {
+    return this.estado;
+  }
 
-     public calcularPrecio():number
-    {
-        //TODO:logica con el precio y el tipo de descuento
-        //por ahora devuelve el precio del combo
-        return this.precio;
-    }
+  public setEstado(estado: EstadoProducto): void {
+    this.estado = estado;
+  }
 }
