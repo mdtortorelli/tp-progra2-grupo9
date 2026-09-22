c8ce04d96b86ea3719b22d731467e836d7f464a8
Juan pablo <lucasjuanpablo1@gmail.com>
2026-09-22 19:48:09 -0300

    Se actualizo mesa

--- src/Mesa.ts
+++ src/Mesa.ts
@@ -1,3 +1,15 @@
 export class Mesa {
-    
+  private numero: number;
+
+  public constructor(numero: number) {
+    this.numero = numero;
+  }
+
+  public getNumero(): number {
+    return this.numero;
+  }
+
+  public setNumero(numero: number): void {
+    this.numero = numero;
+  }
 }
