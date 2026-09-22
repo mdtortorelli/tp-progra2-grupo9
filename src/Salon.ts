83cadc4446af233b3865833cbf5dcb5fc2867f8d
Juan pablo <lucasjuanpablo1@gmail.com>
2026-09-22 19:49:02 -0300

    Se implemento salon y sus cosas

--- src/Salon.ts
+++ src/Salon.ts
@@ -1,17 +1,39 @@
+import { Pedido } from "./Pedido";
+import { Cliente } from "./Cliente";
+import MetodoPago from "./MetodoPago";
+import { AtencionPedido } from "./AtencionPedido";
 import { Mesa } from "./Mesa";
+import { EstadoPedido } from "./EstadoPedido";
 
-export class Salon {
-    private mesa: Mesa;
+export class Salon extends Pedido implements AtencionPedido {
+  private mesa: Mesa;
 
-    public constructor(mesa:Mesa) {
-        this.mesa = mesa;
-    }
-    
-    public getMesa():Mesa {
-        return this.mesa;
-    }
+  public constructor(cliente: Cliente, metodoDePago: MetodoPago, mesa: Mesa) {
+    super(cliente, [], metodoDePago);
+    this.mesa = mesa;
+  }
 
-    public setMesa(mesa:Mesa):void {
-        this.mesa = mesa;
-    }
+  public getMesa(): Mesa {
+    return this.mesa;
+  }
+
+  public setMesa(mesa: Mesa): void {
+    this.mesa = mesa;
+  }
+
+  public eliminarItem(): void {
+    this.deshacerUltimaModificacion();
+  }
+
+  public obtenerEstadoPedido(): EstadoPedido {
+    return this.getEstado();
+  }
+
+  public consultarEstadosDeItems(): void {
+    this.obtenerEstadosItems();
+  }
+
+  public confirmarPedido(): void {
+    this.confirmar();
+  }
 }
